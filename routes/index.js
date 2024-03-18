d1983309d3ac4ddd2e0f00ba159b95b1bcb9dd85
Irtiza <zirtiza@gmail.com>
2024-03-18 22:39:27 +0500

    basic seller and freelancer functions added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const authRouter = require("./auth");
 const projectRouter = require("./project");
@@ -10,15 +10,15 @@ router.use("/auth", authRouter);
 /******* above are all the routes that WILL NOT pass through the middleware ********/
 
 router.use(async (req, res, next) => {
-    try {
-        const token = req.headers.authorization;
-        const user = jwt.verify(token.split(" ")[1], "MY_SECRET")
-        req.user = user;
-        next()
-    } catch (e) {
-        return res.json({ msg: "TOKEN NOT FOUND / INVALID" })
-    }
-})
+  try {
+    const token = req.headers.authorization;
+    const user = jwt.verify(token.split(" ")[1], "MY_SECRET");
+    req.user = user;
+    next();
+  } catch (e) {
+    return res.json({ msg: "TOKEN NOT FOUND / INVALID" });
+  }
+});
 
 /******* below are all the routes that WILL pass through the middleware ********/
 
