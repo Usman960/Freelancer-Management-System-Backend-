c19f10d276b721046b69e04c784864fc9ef11184
Usman960 <mu943498@gmail.com>
2024-03-29 10:45:20 +0500

    changed db name

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 //localhost:27017
 (async () => {
     try {
-        await mongoose.connect("mongodb://0.0.0.0:27017/test")
+        await mongoose.connect("mongodb://0.0.0.0:27017/FreelancerManagementSystem")
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
